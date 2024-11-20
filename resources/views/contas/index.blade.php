5093081142ef275cd7c2334c0beb5856822f8fd6
Antony Chagas <Antony@DESKTOP-NQIS788>
2024-11-20 19:41:27 -0300

    Funcionalidade deletar implementada com sucesso com mensagem de confirmação

--- resources/views/contas/index.blade.php
+++ resources/views/contas/index.blade.php
@@ -1,27 +1,41 @@
 @extends('html')
 @section('conteudo')
 
-<body>
+    <body>
 
-    <a href="{{ route('conta.create') }}"> Cadastrar </a>
-    <h2>Listar as contas</h2>
+        <a href="{{ route('conta.create') }}">
+            <button type="button">Cadastrar</button>
+        </a>
+        <h2>Listar as contas</h2>
 
-    {{--<a href="{{ route('conta.show') }}"> Visualizar </a> <br>
-    <a href="{{ route('conta.edit') }}"> Editar </a> <br>
-    <a href="{{ route('conta.destroy') }}"> Deletar </a> <br>--}}
+        @if (session('sucesso'))
+            <span style="color: #082;">
+                {{ session('sucesso') }}
+            </span> <br><br>
+        @endif
 
-    @forelse ($contas as $conta)
-        ID: {{ $conta->id}} <br>
-        Nome: {{ $conta->nome}} <br>
-        Valor: {{ 'R$ ' . number_format($conta->valor, 2, ',', '.')}} <br>
-        Vencimento: {{ \Carbon\Carbon::parse($conta->vencimento)->tz('America/Sao_Paulo')->format('d/m/Y')}} <br>
+        @forelse ($contas as $conta)
+            ID: {{ $conta->id }} <br>
+            Nome: {{ $conta->nome }} <br>
+            Valor: {{ 'R$ ' . number_format($conta->valor, 2, ',', '.') }} <br>
+            Vencimento: {{ \Carbon\Carbon::parse($conta->vencimento)->tz('America/Sao_Paulo')->format('d/m/Y') }} <br>
 
-        <a href="{{ route('conta.show', ['conta' => $conta->id]) }}"> Visualizar </a> <br>
-        <a href="{{ route('conta.edit', ['conta' => $conta->id]) }}"> Editar </a>
+            <a href="{{ route('conta.show', ['conta' => $conta->id]) }}">
+                <button type="button">Visualizar</button>
+            </a> <br>
+            <a href="{{ route('conta.edit', ['conta' => $conta->id]) }}">
+                <button type="button">Editar</button>
+            </a>
 
-        <hr>
-    @empty
-        <span style="color: #ff0000">Nenhuma conta encontrada</span>
-    @endforelse
+            <form action="{{ route('conta.destroy', ['conta' => $conta->id]) }}}}" method="POST">
+                @csrf
+                @method('delete')
+                <button type="submit"
+                    onclick="return confirm('Tem certeza que deseja apagar esse registro?')">Apagar</button>
+            </form>
+            <hr>
+        @empty
+            <span style="color: #ff0000">Nenhuma conta encontrada</span>
+        @endforelse
 
-</body>
+    </body>
--- resources/views/contas/show.blade.php
+++ resources/views/contas/show.blade.php
@@ -1,22 +1,28 @@
 @extends('html')
 @section('conteudo')
 
-<body>
-    <a href="{{ route('conta.index') }}">Lista de contas</a> <br>
-    <a href="{{ route('conta.edit', ['conta' => $conta->id]) }}"> Editar </a>
+    <body>
+        <a href="{{ route('conta.index') }}">
+            <button type="button">Lista de contas</button>
+        </a> <br>
 
-    <h2>Detalhe da conta</h2>
 
-    @if(session('sucesso'))
-        <span style="color: #082;">
-       {{ session('sucesso') }}
-        </span> <br><br>
-    @endif
+        <h2>Detalhe da conta</h2>
 
-    ID: {{ $conta->id}} <br>
-    Nome: {{ $conta->nome}} <br>
-    Valor: {{ 'R$ ' . number_format($conta->valor, 2, ',', '.')}} <br>
-    Vencimento: {{ \Carbon\Carbon::parse($conta->vencimento)->tz('America/Sao_Paulo')->format('d/m/Y')}} <br>
-    Criado: {{ \Carbon\Carbon::parse($conta->created_at)->tz('America/Sao_Paulo')->format('d/m/Y - H:i:s')}} <br>
-    Editado: {{ \Carbon\Carbon::parse($conta->updated_at)->tz('America/Sao_Paulo')->format('d/m/Y - H:i:s')}} <br>
-</body>
+        @if (session('sucesso'))
+            <span style="color: #082;">
+                {{ session('sucesso') }}
+            </span> <br><br>
+        @endif
+
+        ID: {{ $conta->id }} <br>
+        Nome: {{ $conta->nome }} <br>
+        Valor: {{ 'R$ ' . number_format($conta->valor, 2, ',', '.') }} <br>
+        Vencimento: {{ \Carbon\Carbon::parse($conta->vencimento)->tz('America/Sao_Paulo')->format('d/m/Y') }} <br>
+        Criado: {{ \Carbon\Carbon::parse($conta->created_at)->tz('America/Sao_Paulo')->format('d/m/Y - H:i:s') }} <br>
+        Editado: {{ \Carbon\Carbon::parse($conta->updated_at)->tz('America/Sao_Paulo')->format('d/m/Y - H:i:s') }} <br>
+        <br>
+        <a href="{{ route('conta.edit', ['conta' => $conta->id]) }}">
+            <button type="button">Editar</button>
+        </a>
+    </body>
