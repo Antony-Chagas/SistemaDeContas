42e6bce0571f1ac70181dfeeca1b9be98a6af6c8
Antony Chagas <Antony@DESKTOP-NQIS788>
2024-11-20 21:15:50 -0300

    Salvamento de log implementado com sucesso.

--- app/Http/Controllers/ContaController.php
+++ app/Http/Controllers/ContaController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\ContaRequest;
 use App\Models\Conta;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ContaController extends Controller
 {
@@ -30,8 +31,10 @@ class ContaController extends Controller
         $request->validated();
         try {
             $conta = Conta::create($request->all());
+            Log::info('Conta criada com sucesso.', ['id' => $conta->id, 'conta' => $conta]);
             return redirect()->route('conta.show', ['conta' => $conta->id])->with('sucesso', 'Conta cadastrada com sucesso!');
         } catch (Exception $e) {
+            Log::warning('Erro ao criar conta.', ['error' => $e->getMessage()]);
             return back()->with('error', 'Conta não cadastrada!');
         }
     }
@@ -50,8 +53,10 @@ class ContaController extends Controller
                 'valor' => $request->valor,
                 'vencimento' => $request->vencimento,
             ]);
+            Log::info('Conta editada com sucesso.', ['id' => $conta->id, 'conta' => $conta]);
             return redirect()->route('conta.show', ['conta' => $conta->id])->with('sucesso', 'Conta editada com sucesso!');
         } catch (Exception $e) {
+            Log::warning('Conta não editada.', ['error' => $e->getMessage()]);
             return back()->withInput()->with('error', 'Conta não editada!');
         }
     }
@@ -60,8 +65,10 @@ class ContaController extends Controller
     {
         try {
             $conta->delete();
+            Log::info('Conta deletada com sucesso.', ['id' => $conta->id, 'conta' => $conta]);
             return redirect()->route('conta.index')->with('sucesso', 'Conta apagada com sucesso!');
         } catch (Exception $e) {
+            Log::warning('Conta não deletada.', ['error' => $e->getMessage()]);
             return back()->with('error', 'Conta não apagada!');
         }
     }
