11acfa5bb7243dd8683633f9c76ae2125fa9fe80
Antony Chagas <Antony@DESKTOP-NQIS788>
2024-11-30 19:16:09 -0300

    layout implementado com sucesso

--- resources/views/contas/create.blade.php
+++ resources/views/contas/create.blade.php
@@ -1,41 +1,61 @@
 @extends('layouts.admin')
 @section('content')
-
-    <body>
-        <a href="{{ route('conta.index') }}">
-            <button type="button">Lista de contas</button>
-        </a>
-        <h2>Cadastar as contas</h2>
-
+    <div class="card mt-4 mb-4  border-light shadow">
+        <div class="card-header d-flex justify-content-between">
+            <span>Cadastrar conta</span>
+            <span>
+                <a href="{{ route('conta.index') }}" type="button" class="btn btn-info btn-sm">
+                    Listar
+                </a>
+            </span>
+        </div>
         @if (session('error'))
-            <span style="color: #ff0000;">
+            <div class="alert alert-danger m-3" role="alert">
                 {{ session('error') }}
-            </span> <br><br>
+            </div>
+        @endif
+
+        @if (session('sucesso'))
+            <div class="alert alert-success m-3" role="alert">
+                {{ session('sucesso') }}
+            </div>
         @endif
 
         @if ($errors->any())
-            <span style="color: #ff0000;">
-                @foreach ($errors->all() as $error)
-                    {{ $error }}<br>
-                @endforeach
-            </span>
-            <br>
-        @endif
+        <div class="alert alert-danger m-3" role="alert">
+            @foreach ($errors->all() as $error)
+                {{ $error }}<br>
+            @endforeach
+        </div>
+    @endif
 
-        <form action="{{ route('conta.store') }}" method="POST">
-            @csrf
-            <label>Nome: </label>
-            <input type="text" name="nome" id="nome" placeholder="Nome da conta" value="{{ old('nome') }}">
-            <br><br>
+        <div class="card-body">
+            <form action="{{ route('conta.store') }}" method="POST" class="row g-3">
+                @csrf
 
-            <label>Valor: </label>
-            <input type="text" name="valor" id="valor" placeholder="Valor da conta" value="{{ old('valor') }}">
-            <br><br>
+                <div class="col-12">
+                    <label for="nome" class="form-label">Nome</label>
+                    <input type="text" name="nome" class="form-control" id="nome" placeholder="Nome da conta"
+                        value="{{ old('nome') }}">
+                </div>
 
-            <label>Vencimento: </label>
-            <input type="date" name="vencimento" id="vencimento" value="{{ old('vencimento') }}"> <br><br>
+                <div class="col-12">
+                    <label for="Valor" class="form-label">Valor </label>
+                    <input type="text" name="valor" class="form-control" id="valor" placeholder="Valor da conta"
+                        value="{{ old('valor')}}">
+                </div>
 
-            <button type="submit">Cadastrar</button>
-        </form>
-    </body>
+                <div class="col-12">
+                    <label for="vencimento" class="form-label">Vencimento</label>
+                    <input type="date" name="vencimento" class="form-control" id="vencimento"
+                        value="{{ old('vencimento') }}">
+                </div>
+
+                <div class="col-12">
+                <button type="submit" class="btn btn-success btn-sm">Cadastar</button>
+                </div>
+
+            </form>
+        </div>
+    </div>
 @endsection
